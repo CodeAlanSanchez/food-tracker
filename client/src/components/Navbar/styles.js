36e91f843db4b72fe6502b701c4b08208a0259c2
Alan Sanchez <realalansan@gmail.com>
2021-03-29 13:09:45 -0700

    Add styles for components

---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  navigationBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '10px',
+  },
+}));
