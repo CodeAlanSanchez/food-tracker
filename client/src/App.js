dac8047117a6bfc0469ea997b9450eed302cf86d
Alan Sanchez <realalansan@gmail.com>
2021-03-29 13:09:36 -0700

    Add Tracker and Router components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { CssBaseline } from '@material-ui/core';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Tracker from './components/Tracker/Tracker';
 
 const App = () => (
-  <div />
+  <div>
+    <CssBaseline />
+    <Navbar />
+    <main>
+      <Switch>
+        <Route path={['/']} exact component={Tracker} />
+        <Route path="/account" />
+        <Route path="/meals" />
+      </Switch>
+    </main>
+  </div>
 );
 
 export default App;
---
+++ client/src/components/Tracker/Tracker.jsx
@@ -0,0 +1,52 @@
+import {
+  Box, Container, Grid, Grow, Typography,
+} from '@material-ui/core';
+import React from 'react';
+import Info from './Info/Info';
+import MealTime from './MealTime/MealTime';
+import useStyles from './styles';
+
+const Tracker = () => {
+  const classes = useStyles();
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Box mt={3}>
+          <Typography variant="h2" align="center" gutterBottom>
+            Food Tracker
+          </Typography>
+        </Box>
+        <Box className={classes.tracker}>
+          <Grid container justify="center" alignItems="stretch" spacing={0}>
+            <Grid item xs={12} sm={4}>
+              <Container maxWidth="sm">
+                <MealTime
+                  time="Breakfast"
+                  items={[{
+                    name: 'Apple', amount: '1 Cup', calories: 300, drink: false,
+                  }, {
+                    name: 'Potato', amount: '100g', calories: 250, drink: false,
+                  }, {
+                    name: 'Water', amount: '500ml', calories: 0, drink: true,
+                  }]}
+                />
+                <Box mb={2} />
+                <MealTime time="Lunch" items={[]} />
+                <Box mb={2} />
+                <MealTime time="Dinner" items={[]} />
+                <Box mb={2} />
+                <MealTime time="Snacks" items={[]} />
+              </Container>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Info />
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Tracker;
