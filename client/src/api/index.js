b8d906692386f0ebf02e849cac19501d5dc59fb9
Alan Sanchez <realalansan@gmail.com>
2021-03-31 04:22:38 -0700

    Update url in API file

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const url = 'http://localhost:5000';
 
-export const fetchMeals = () => axios.get(url);
-export const fetchMeal = (id) => axios.get(`${url}:${id}`);
-export const createMeal = (id, meal) => axios.create(`${url}:${id}`, meal);
-export const updateMeal = (id, meal) => axios.patch(`${url}:${id}`, meal);
-export const deleteMeal = (id) => axios.delete(`${url}:${id}`);
+export const fetchMeals = () => axios.get(`${url}/meals`);
+export const fetchMeal = (id) => axios.get(`${url}/meals/${id}`);
+export const createMeal = (meal) => axios.post(`${url}/meals`, meal);
+export const updateMeal = (id, meal) => axios.patch(`${url}/meals/${id}`, meal);
+export const deleteMeal = (id) => axios.delete(`${url}/meals/${id}`);
