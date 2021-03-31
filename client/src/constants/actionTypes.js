62438ad52a1d96c127d54cabc4556effba034cbe
Alan Sanchez <realalansan@gmail.com>
2021-03-30 20:15:25 -0700

    Add reducer and action constants

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const CREATE_MEAL = 'CREATE_MEAL';
+export const UPDATE_MEAL = 'UPDATE_MEAL';
+export const DELETE_MEAL = 'DELETE_MEAL';
+export const FETCH_MEALS = 'FETCH_MEALS';
+export const FETCH_MEAL = 'FETCH_MEAL';
---
+++ client/src/reducers/mealReducers.js
@@ -0,0 +1,19 @@
+import {
+  CREATE_MEAL, UPDATE_MEAL, DELETE_MEAL, FETCH_MEALS, FETCH_MEAL,
+} from '../constants/actionTypes';
+
+export default (meals = [], action) => {
+  switch (action.type) {
+    default:
+      return meals;
+    case FETCH_MEALS:
+    case FETCH_MEAL:
+      return action.payload;
+    case CREATE_MEAL:
+      return [...meals, action.payload];
+    case UPDATE_MEAL:
+      return meals.map((meal) => (meal._id === action.payload._id ? action.payload : meal));
+    case DELETE_MEAL:
+      return action.payload;
+  }
+};
