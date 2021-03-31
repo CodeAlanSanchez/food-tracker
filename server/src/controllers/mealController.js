bb400a4c7552da48e55f487b9bcb3e7d3c18c0b5
Alan Sanchez <realalansan@gmail.com>
2021-03-31 04:24:17 -0700

    Fix typo in get meals controller

--- server/src/controllers/mealController.js
+++ server/src/controllers/mealController.js
@@ -5,7 +5,7 @@ export const getMeals = async (req, res) => {
   try {
     const data = await Meal.find();
 
-    res.status.json(data);
+    res.status(200).json(data);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
