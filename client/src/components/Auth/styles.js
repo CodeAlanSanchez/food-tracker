cde3640743ae9224f3237c1ebd6381cd1e1a0b95
Alan Sanchez <realalansan@gmail.com>
2021-03-31 17:49:26 -0700

    Add styles for auth

---
+++ client/src/components/Auth/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  auth: {
+    marginTop: theme.spacing(10),
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: theme.spacing(2),
+  },
+}));
