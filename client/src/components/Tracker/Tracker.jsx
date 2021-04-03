ed1e37ac125acb405f31bd7927cd3bb1c60fe0f2
Alan Sanchez <realalansan@gmail.com>
2021-04-03 01:00:25 -0700

    Add meals from state to children

--- client/src/components/Tracker/Tracker.jsx
+++ client/src/components/Tracker/Tracker.jsx
@@ -2,12 +2,14 @@ import {
   Box, Container, Grid, Grow, Typography,
 } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Info from './Info/Info';
 import MealTime from './MealTime/MealTime';
 import useStyles from './styles';
 
 const Tracker = () => {
   const classes = useStyles();
+  const meals = useSelector((state) => state.meals);
 
   return (
     <Grow in>
@@ -23,18 +25,18 @@ const Tracker = () => {
               <Container maxWidth="sm">
                 <MealTime
                   time="Breakfast"
-                  items={[]}
+                  items={meals.filter((meal) => meal.category === 'Breakfast')}
                 />
                 <Box mb={2} />
-                <MealTime time="Lunch" items={[]} />
+                <MealTime time="Lunch" items={meals.filter((meal) => meal.category === 'Lunch')} />
                 <Box mb={2} />
-                <MealTime time="Dinner" items={[]} />
+                <MealTime time="Dinner" items={meals.filter((meal) => meal.category === 'Dinner')} />
                 <Box mb={2} />
-                <MealTime time="Snacks" items={[]} />
+                <MealTime time="Snacks" items={meals.filter((meal) => meal.category === 'Snacks')} />
               </Container>
             </Grid>
             <Grid item xs={12} sm={3}>
-              <Info />
+              <Info items={meals} />
             </Grid>
           </Grid>
         </Box>
