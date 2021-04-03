9180a58e3070c1f82e05d28045a3c88a195b1456
Alan Sanchez <realalansan@gmail.com>
2021-04-03 01:01:35 -0700

    Add auth reducer to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,9 +2,10 @@ import {
   createStore, applyMiddleware, compose, combineReducers,
 } from 'redux';
 import thunk from 'redux-thunk';
+import authReducers from '../reducers/authReducers';
 import mealReducers from '../reducers/mealReducers';
 import timeReducers from '../reducers/timeReducers';
 
-const reducers = combineReducers({ meals: mealReducers, time: timeReducers });
+const reducers = combineReducers({ meals: mealReducers, time: timeReducers, auth: authReducers });
 
 export default createStore(reducers, compose(applyMiddleware(thunk)));
