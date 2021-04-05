d5194cadf002e850f4ac0540f4a367669578b3f0
Alan Sanchez <realalansan@gmail.com>
2021-04-04 23:59:08 -0700

    Add log reducer

---
+++ client/src/reducers/logReducers.js
@@ -0,0 +1,10 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_LOG':
+      return state;
+    case 'UPDATE_LOG':
+      return state;
+    default:
+      return state;
+  }
+};
