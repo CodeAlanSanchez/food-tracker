ed2ca75ab4886a833d2412c6f148e2d8b4b37c09
Alan Sanchez <realalansan@gmail.com>
2021-04-06 03:28:27 -0700

    Add default and required to model

--- server/src/models/logModel.js
+++ server/src/models/logModel.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const logSchema = new mongoose.Schema({
   creator: String,
-  date: {type: String, required: true, default: new Date()},
-  breakfast: [String],
-  lunch: [String],
-  dinner: [String],
-  snacks: [String],
+  date: { type: String, required: true, default: new Date() },
+  breakfast: { type: [String], required: true, default: [] },
+  lunch: { type: [String], required: true, default: [] },
+  dinner: { type: [String], required: true, default: [] },
+  snacks: { type: [String], required: true, default: [] },
 })
 
 export default mongoose.model('log', logSchema);
