1e42734ce729313cc5347fb03ab0228d5acdfad6
Alan Sanchez <realalansan@gmail.com>
2021-04-06 03:28:00 -0700

    Fix route

--- server/src/routes/logRoutes.js
+++ server/src/routes/logRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getLogs, createLog, updateLog } from '../controllers/logController';
+import { getLogs, createLog, updateLog } from '../controllers/logController.js';
 
 const router = express.Router();
 
-router.get('/', getMeals);
-router.post('/:id', createMeal);
-router.patch('/:id', updateMeal);
+router.get('/', getLogs);
+router.post('/', createLog);
+router.patch('/:id', updateLog);
+
+export default router;
