8a8d596cf3c65e826a744f753edf59d80969214c
Alan Sanchez <realalansan@gmail.com>
2021-04-06 03:26:25 -0700

    Check if date of log is today

--- client/src/components/Tracker/Tracker.jsx
+++ client/src/components/Tracker/Tracker.jsx
@@ -1,15 +1,29 @@
 import {
-  Box, Container, Grid, Grow, Typography,
+  Box, CircularProgress, Container, Grid, Grow, Typography,
 } from '@material-ui/core';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Info from './Info/Info';
 import MealTime from './MealTime/MealTime';
 import useStyles from './styles';
+import { createLog } from '../../actions/logActions';
 
 const Tracker = () => {
   const classes = useStyles();
-  const meals = useSelector((state) => state.meals);
+  const logs = useSelector((state) => state.logs);
+  const currentLog = logs.filter((log) => {
+    if
+    (new Date(log?.date) === new Date().getDay()) { return log; }
+    return '';
+  });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!currentLog?.breakfast) {
+      dispatch(createLog({}));
+      console.log('CREATING');
+    }
+  }, [dispatch]);
 
   return (
     <Grow in>
@@ -21,23 +35,34 @@ const Tracker = () => {
         </Box>
         <Box className={classes.tracker}>
           <Grid container justify="center" alignItems="stretch" spacing={0}>
-            <Grid item xs={12} sm={4}>
-              <Container maxWidth="sm">
-                <MealTime
-                  time="Breakfast"
-                  items={meals.filter((meal) => meal.category === 'Breakfast')}
-                />
-                <Box mb={2} />
-                <MealTime time="Lunch" items={meals.filter((meal) => meal.category === 'Lunch')} />
-                <Box mb={2} />
-                <MealTime time="Dinner" items={meals.filter((meal) => meal.category === 'Dinner')} />
-                <Box mb={2} />
-                <MealTime time="Snacks" items={meals.filter((meal) => meal.category === 'Snacks')} />
-              </Container>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <Info items={meals} />
-            </Grid>
+            {
+              currentLog?.breakfast ? (
+                <>
+                  <Grid item xs={12} sm={4}>
+                    <Container maxWidth="sm">
+                      <MealTime time="Breakfast" items={currentLog?.breakfast} />
+                      <Box mb={2} />
+                      <MealTime time="Lunch" items={currentLog?.lunch} />
+                      <Box mb={2} />
+                      <MealTime time="Dinner" items={currentLog?.dinner} />
+                      <Box mb={2} />
+                      <MealTime time="Snacks" items={currentLog?.snacks} />
+                    </Container>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Info items={[
+                      ...currentLog?.breakfast,
+                      ...currentLog?.lunch,
+                      ...currentLog?.dinner,
+                      ...currentLog?.snacks]}
+                    />
+                  </Grid>
+                </>
+              )
+                : (
+                  <CircularProgress />
+                )
+            }
           </Grid>
         </Box>
       </Container>
