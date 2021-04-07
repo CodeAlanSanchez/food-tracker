3d8790d2ce1f3c97ca67734ee9b67923d734a10e
Alan Sanchez <realalansan@gmail.com>
2021-04-07 04:47:44 -0700

    Add specific actions

--- client/src/actions/currentLogActions.js
+++ client/src/actions/currentLogActions.js
@@ -1,8 +1,23 @@
-import * as api from '../api';
-import { ADD_MEAL, GET_MEALS, REMOVE_MEAL } from '../constants/actionTypes';
+import {
+  ADD_BREAKFAST, ADD_DINNER, ADD_LUNCH,
+  ADD_SNACK, GET_MEALS, REMOVE_BREAKFAST,
+  REMOVE_LUNCH, REMOVE_DINNER, REMOVE_SNACK,
+} from '../constants/actionTypes';
 
 export const getCurrentMeals = () => ({ type: GET_MEALS });
 
-export const addCurrentMeal = (meal) => ({ type: ADD_MEAL, payload: meal });
+export const addBreakfast = (meal) => ({ type: ADD_BREAKFAST, payload: meal });
 
-export const removeCurrentMeal = (id) => ({ type: REMOVE_MEAL, payload: id });
+export const addLunch = (meal) => ({ type: ADD_LUNCH, payload: meal });
+
+export const addDinner = (meal) => ({ type: ADD_DINNER, payload: meal });
+
+export const addSnack = (meal) => ({ type: ADD_SNACK, payload: meal });
+
+export const removeBreakfast = (id) => ({ type: REMOVE_BREAKFAST, payload: id });
+
+export const removeLunch = (id) => ({ type: REMOVE_LUNCH, payload: id });
+
+export const removeDinner = (id) => ({ type: REMOVE_DINNER, payload: id });
+
+export const removeSnack = (id) => ({ type: REMOVE_SNACK, payload: id });
--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -15,5 +15,12 @@ export const CREATE_LOG = 'CREATE_LOG';
 export const UPDATE_LOG = 'UPDATE_LOG';
 
 export const GET_MEALS = 'GET_MEALS';
-export const ADD_MEAL = 'ADD_MEAL';
-export const REMOVE_MEAL = 'REMOVE_MEAL';
+export const ADD_BREAKFAST = 'ADD_BREAKFAST';
+export const ADD_LUNCH = 'ADD_LUNCH';
+export const ADD_DINNER = 'ADD_DINNER';
+export const ADD_SNACK = 'ADD_SNACK';
+
+export const REMOVE_BREAKFAST = 'REMOVE_BREAKFAST';
+export const REMOVE_LUNCH = 'REMOVE_LUNCH';
+export const REMOVE_DINNER = 'REMOVE_DINNER';
+export const REMOVE_SNACK = 'REMOVE_SNACK';
