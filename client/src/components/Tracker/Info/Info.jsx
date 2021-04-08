53da447356746bd47e4a54ea6164d98be9451ba5
Alan Sanchez <realalansan@gmail.com>
2021-04-08 06:41:13 -0700

    Have Info use the state to show nutrients

--- client/src/components/Tracker/Info/Info.jsx
+++ client/src/components/Tracker/Info/Info.jsx
@@ -2,46 +2,51 @@ import {
   Box, List, ListItem, ListItemText, Paper, Typography,
 } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Info = ({ items = [] }) => (
-  <Paper position="static">
-    <Box display="flex" flexDirection="column" justifyContent="left" p={2}>
-      <Typography variant="h4" gutterBottom>
-        Today
-      </Typography>
-    </Box>
-    <Box p={2} pt={0}>
-      <Typography variant="h5">
-        Calories
-      </Typography>
-      <List>
-        <ListItem>
-          <ListItemText primary={items.reduce((acc, item) => acc + item.calories, 0)} />
-        </ListItem>
-      </List>
-      <Typography variant="h5">
-        Macros
-      </Typography>
-      <List>
-        <ListItem>
-          <ListItemText primary="Protein" />
-          <ListItemText primary={`${items.reduce((acc, item) => acc + item.protein, 0)}g`} align="right" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Carbohydrates" />
-          <ListItemText primary={`${items.reduce((acc, item) => acc + item.carbohydrates, 0)}g`} align="right" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Fat" />
-          <ListItemText primary={`${items.reduce((acc, item) => acc + item.fat, 0)}g`} align="right" />
-        </ListItem>
-        <ListItem>
-          <ListItemText primary="Fiber" />
-          <ListItemText primary={`${items.reduce((acc, item) => acc + item.fiber, 0)}g`} align="right" />
-        </ListItem>
-      </List>
-    </Box>
-  </Paper>
-);
+const Info = ({ items = [] }) => {
+  const currentLog = useSelector((state) => state.currentLog);
+
+  return (
+    <Paper position="static">
+      <Box display="flex" flexDirection="column" justifyContent="left" p={2}>
+        <Typography variant="h4" gutterBottom>
+          Today
+        </Typography>
+      </Box>
+      <Box p={2} pt={0}>
+        <Typography variant="h5">
+          Calories
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary={items.reduce((acc, item) => acc + item.calories, 0)} />
+          </ListItem>
+        </List>
+        <Typography variant="h5">
+          Macros
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemText primary="Protein" />
+            <ListItemText primary={`${items.reduce((acc, item) => acc + item.protein, 0)}g`} align="right" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Carbohydrates" />
+            <ListItemText primary={`${items.reduce((acc, item) => acc + item.carbohydrates, 0)}g`} align="right" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Fat" />
+            <ListItemText primary={`${items.reduce((acc, item) => acc + item.fat, 0)}g`} align="right" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="Fiber" />
+            <ListItemText primary={`${items.reduce((acc, item) => acc + item.fiber, 0)}g`} align="right" />
+          </ListItem>
+        </List>
+      </Box>
+    </Paper>
+  );
+};
 
 export default Info;
--- client/src/components/Tracker/Tracker.jsx
+++ client/src/components/Tracker/Tracker.jsx
@@ -19,21 +19,6 @@ const Tracker = () => {
   const user = JSON.stringify(localStorage.getItem('profile'));
   const history = useHistory();
 
-  useEffect(() => {
-    if (!user) {
-      history.push('/auth');
-    }
-    console.log(currentLog);
-    if (currentLog !== null && currentLog?.breakfast) {
-      setItems([
-        ...currentLog?.breakfast,
-        ...currentLog?.lunch,
-        ...currentLog?.dinner,
-        ...currentLog?.snacks,
-      ]);
-    }
-  }, []);
-
   return (
     <Grow in>
       <Container maxWidth="xl">
@@ -56,7 +41,7 @@ const Tracker = () => {
               </Container>
             </Grid>
             <Grid item xs={12} sm={3}>
-              <Info items={items} />
+              <Info />
             </Grid>
           </Grid>
         </Box>
