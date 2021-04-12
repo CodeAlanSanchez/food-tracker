c613f334a6d28a20f2fad7a8ff8b88b3f96ecaf8
Alan Sanchez <realalansan@gmail.com>
2021-04-12 15:28:22 -0700

    Fix date checking

--- server/src/controllers/logController.js
+++ server/src/controllers/logController.js
@@ -15,14 +15,17 @@ export const createLog = async (req, res) => {
   const log = req.body;
   try {
     const existingLogs = await Log.find({ creator: req.userId });
+    console.log(existingLogs);
 
     const today = new Date();
 
-    const existingLog = existingLogs.find((log) => log.date.getFullYear() === today.getFullYear() &&
-    log.date.getMonth() === today.getMonth() &&
-    log.date.getDate() === today.getDate())
-
-    if (existingLog) return res.status(200).json(existingLog);
+    if (existingLogs) {
+      const oldDate = new Date(log.date);
+      const existingLog = existingLogs.find((log) => oldDate.getFullYear() === today.getFullYear &&
+      oldDate.getMonth() === today.getMonth() &&
+      oldDate.getDate() === today.getDate())
+      return res.status(200).json(existingLog);
+    }
 
     const savedLog = new Log({ ...log, creator: req.userId, date: new Date() });
 
