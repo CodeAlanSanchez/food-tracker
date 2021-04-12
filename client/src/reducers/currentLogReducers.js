322e1da360b355640e417f99962293e0eda24c5d
Alan Sanchez <realalansan@gmail.com>
2021-04-12 03:33:57 -0700

    Fix reducer for removing items

--- client/src/reducers/currentLogReducers.js
+++ client/src/reducers/currentLogReducers.js
@@ -12,21 +12,33 @@ export default (current = {
     case CREATE_LOG:
       return action.payload;
     case ADD_BREAKFAST:
-      return { ...current, breakfast: [...current.breakfast, action.payload] };
+      return { ...current, breakfast: [...current?.breakfast, action.payload] };
     case ADD_LUNCH:
-      return { ...current, lunch: [...current.lunch, action.payload] };
+      return { ...current, lunch: [...current?.lunch, action.payload] };
     case ADD_DINNER:
-      return { ...current, dinner: [...current.dinner, action.payload] };
+      return { ...current, dinner: [...current?.dinner, action.payload] };
     case ADD_SNACK:
-      return { ...current, snacks: [...current.snacks, action.payload] };
+      return { ...current, snacks: [...current?.snacks, action.payload] };
     case REMOVE_BREAKFAST:
-      return current.breakfast.filter((curr) => curr._id !== action.payload);
+      return {
+        ...current,
+        breakfast: current.breakfast.filter((curr) => curr._id !== action.payload),
+      };
     case REMOVE_LUNCH:
-      return current.lunch.filter((curr) => curr._id !== action.payload);
+      return {
+        ...current,
+        lunch: current.lunch.filter((curr) => curr._id !== action.payload),
+      };
     case REMOVE_DINNER:
-      return current.dinner.filter((curr) => curr._id !== action.payload);
+      return {
+        ...current,
+        dinner: current.dinner.filter((curr) => curr._id !== action.payload),
+      };
     case REMOVE_SNACK:
-      return current.snacks.filter((curr) => curr._id !== action.payload);
+      return {
+        ...current,
+        snacks: current.snacks.filter((curr) => curr._id !== action.payload),
+      };
     default:
       return current;
   }
