b54cb46a0f915ca76432fbb146ee5e778b991782
Alan Sanchez <realalansan@gmail.com>
2021-05-18 13:33:35 -0700

    Add dotenv to axios URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const API = axios.create({ baseURL: BASE_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
