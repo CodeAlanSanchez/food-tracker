6e0b42ce4828b965baec47073111d44e932e2a68
Alan Sanchez <realalansan@gmail.com>
2021-05-19 08:49:19 -0700

    Fix authorization

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -2,7 +2,8 @@ import jwt, {decode} from 'jsonwebtoken';
 
 export default (req, res, next) => {
   try {
-    const token = req.headers?.authorization.split(' ')[1];
+    if (!req.headers.hasOwnProperty('authorization')) return;
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) return res.status(401).send('Access denied.');
     let decodedToken;
     if (token) {
