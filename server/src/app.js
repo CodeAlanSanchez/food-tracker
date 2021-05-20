bfb003c540de20bf2c78a88c69120a9eeee2ae5f
Alan Sanchez <realalansan@gmail.com>
2021-05-19 17:22:39 -0700

    Fix PORT

--- server/src/app.js
+++ server/src/app.js
@@ -19,7 +19,7 @@ app.use('/auth', userRoutes);
 app.use('/logs', logRoutes)
 
 const { CONNECTION_URI } = process.env;
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
