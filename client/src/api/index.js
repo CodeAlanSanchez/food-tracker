10fadc20331a61872d3fb5f725c7452bd1a4d536
Alan Sanchez <realalansan@gmail.com>
2021-08-18 04:28:21 -0700

    Fix URI for axios requests

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -12,15 +12,15 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export const fetchMeals = () => API.get('/meals');
-export const fetchMeal = (id) => API.get(`/meals/${id}`);
-export const createMeal = (meal) => API.post('/meals', meal);
-export const updateMeal = (id, meal) => API.patch(`/meals/${id}`, meal);
-export const deleteMeal = (id) => API.delete(`/meals/${id}`);
+export const fetchMeals = () => API.get(`${BASE_URL}/meals`);
+export const fetchMeal = (id) => API.get(`${BASE_URL}/meals/${id}`);
+export const createMeal = (meal) => API.post(`${BASE_URL}/meals`, meal);
+export const updateMeal = (id, meal) => API.patch(`${BASE_URL}/meals/${id}`, meal);
+export const deleteMeal = (id) => API.delete(`${BASE_URL}/meals/${id}`);
 
-export const signIn = (formData) => API.post('/auth/signin', formData);
-export const signUp = (formData) => API.post('/auth/signup', formData);
+export const signIn = (formData) => API.post(`${BASE_URL}/auth/signin`, formData);
+export const signUp = (formData) => API.post(`${BASE_URL}/auth/signup`, formData);
 
-export const fetchLogs = () => API.get('/logs');
-export const createLog = (logData) => API.post('/logs', logData);
-export const updateLog = (id, logData) => API.patch(`/logs/${id}`, logData);
+export const fetchLogs = () => API.get(`${BASE_URL}/log`);
+export const createLog = (logData) => API.post(`${BASE_URL}/log`, logData);
+export const updateLog = (id, logData) => API.patch(`${BASE_URL}/logs/${id}`, logData);
